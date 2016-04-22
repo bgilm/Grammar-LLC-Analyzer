483d7b1a4c1046069667c7e11b093b5177a1c38c
loresfca <loresfca.flores1@gmail.cpm>
2016-04-21 21:36:48 -0500

    Formato correcto de impresión

--- src/Procesamiento.java
+++ src/Procesamiento.java
@@ -20,8 +20,6 @@ public class Procesamiento {
 			if(res.equals(cadena)){
 				aceptado=true;
 				flag=false;
-				System.out.println(n);
-
 				automataPila(n);
 				break;
 			}
@@ -68,56 +66,54 @@ public class Procesamiento {
 	}
 	public void automataPila(Nodo n){
 		String cadenaAux=cadena;
+		if(cadenaAux.equals("")){
+			cadenaAux="λ";
+		}
 		Stack<String> pila = new Stack<String>();
 		Stack<String> resultado = new Stack<String>();
 
 		Nodo aux=n;
 		while(aux!=null){
 			pila.push(aux.getProduccionUsada());
-
 			aux=aux.getAnterior();
 		}
 		pila.push("Z");
-		//System.out.println(pila);
-
-		//Set<String> mySet = new HashSet<String>(Arrays.asList(pila.pop().split("")));
 		resultado.push(pila.pop());
-		System.out.println("<q0,"+cadenaAux+","+resultado+">");
+		System.out.println("<q0,"+cadenaAux+","+imprimirResultado(resultado)+">");
 		resultado.push(pila.pop());
-		System.out.println("<q1,"+cadenaAux+","+resultado+">");
-
+		System.out.println("<q1,"+cadenaAux+","+imprimirResultado(resultado)+">");
 		while(!resultado.peek().equals("Z")){
 			if(gramatica.getAlfabeto().contains(resultado.peek())){
 				resultado.pop();
 				cadenaAux=cadenaAux.substring(1, cadenaAux.length());
-				System.out.println("<q1,"+cadenaAux+","+resultado+">");
-
+				if(cadenaAux.equals("")){
+					cadenaAux="λ";
+				}
+				System.out.println("<q1,"+cadenaAux+","+imprimirResultado(resultado)+">");
 			}else{
-
 				resultado.pop();
-
-				ArrayList<String>reversed=reverse(pila.pop().split(""));
-
-
-				//				List<String> list = new ArrayList<String>(mySet);
-				//				System.out.println(list);
-				//				Collections.sort(list, Collections.reverseOrder());
-				//				System.out.println(list);
-				resultado.addAll(reversed);
-
-				System.out.println("<q1,"+cadenaAux+","+resultado+">");
+				if(!pila.isEmpty()){
+					ArrayList<String>reversed=reverse(pila.pop().split(""));
+					resultado.addAll(reversed);
+					System.out.println("<q1,"+cadenaAux+","+imprimirResultado(resultado)+">");
+				}	
 			}
 		}
-		System.out.println("<q2,"+","+resultado+">");
+		System.out.println("<q2,"+"λ,"+imprimirResultado(resultado)+">");
 	}
 	public ArrayList<String> reverse (String[]arr){
 		ArrayList<String>res= new ArrayList<String>();
 		for(int i=arr.length-1;i>-1;i--){
-
 			res.add(arr[i]);
 		}
-
 		return res;
+	}
+	public String imprimirResultado(Stack<String> r){
+		String s="";
+		for(String st:r){
+			s=st+s;
+		}
+		return s;
 	}
 
 }
